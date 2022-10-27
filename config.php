648084f962405710a90b0d38e62a89e032fe2357
itsTari <tariebiglorysamuel@gmail.com>
2022-10-27 15:47:15 +0100

    Add your commit message

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php  
+    /* Database Connection */  
+    $sDbHost = 'localhost' ;  
+    $sDbName = 'gt_register' ;  
+    $sDbUser = 'root';  
+    $sDbPud = '';  
+    $ Conn = mysql_connect ($sDbHost, $sDbUser, $sDbPwd);  
+    mysql_select_db ($sDbName, $Conn);
+    
+?>  
+      
+    
+
+  
